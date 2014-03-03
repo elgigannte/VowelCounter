784f3ba9aa70c8eda0e604e11fdaa3ac9c9552a5
Stephanie Guilbert <stephanie.guilbert@gmail.com>
2014-03-02 20:59:40 -0500

    Fixer expression régulière pour exclure les virgules (,) du match

--- src/vowelcounter/VowelCounter.java
+++ src/vowelcounter/VowelCounter.java
@@ -4,7 +4,7 @@ package vowelcounter;
 public class VowelCounter {
 
     public static int vowelCount(String originalString){
-        String stringWithoutConsonant = originalString.replaceAll("(?i)[^a,e,i,o,u,y]", "");
+        String stringWithoutConsonant = originalString.replaceAll("(?i)[^aeiouy]", "");
         return stringWithoutConsonant.length();
     }
 }
