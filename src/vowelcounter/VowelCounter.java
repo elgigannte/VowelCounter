24865b47fa251dd63973a5372a037f9317d8439a
Stephanie Guilbert <stephanie.guilbert@gmail.com>
2014-03-04 21:01:03 -0500

    Ajout d'une vérification pour un paramètre null

--- src/vowelcounter/VowelCounter.java
+++ src/vowelcounter/VowelCounter.java
@@ -4,6 +4,10 @@ package vowelcounter;
 public class VowelCounter {
 
     public static int vowelCount(String originalString){
+        if(originalString == null){
+            return 0;
+        }
+        
         String stringWithoutConsonant = originalString.replaceAll("(?i)[^aeiouy]", "");
         return stringWithoutConsonant.length();
     }
